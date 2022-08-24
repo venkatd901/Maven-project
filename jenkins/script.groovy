35fa20d1d700d1b3c7cb66607c82baf6eea3d062
Mohit-dev5 <62936886+Mohit-dev5@users.noreply.github.com>
2022-08-24 16:11:42 +0530

    Updated the file to build and push image into ACR

--- jenkins/script.groovy
+++ jenkins/script.groovy
@@ -5,12 +5,21 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        //withCredentials([usernamePassword(credentialsId: 'ACR', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        //sh 'docker build -t advisingbank/demo-app .'
+        //sh "echo $PASS | docker login -u $USER --password-stdin"
+        //sh 'docker push nanajanashia/demo-app:jma-2.0'
+        dockerImage = docker.build registryName
     }
 } 
+
+def pushImage() {
+    echo 'Pushing the build image...'
+         docker.withRegistry( "http://${registryUrl}", registryCredential ) {
+         dockerImage.push()
+            
+} 
+
 
 def deployApp() {
     echo 'deploying the application...'
