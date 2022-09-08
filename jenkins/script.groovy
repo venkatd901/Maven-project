a6871270b7b657857ae46ddfe55fca4aa8bbb783
Mohit-dev5 <62936886+Mohit-dev5@users.noreply.github.com>
2022-09-08 11:39:18 +0530

    Update script.groovy

--- jenkins/script.groovy
+++ jenkins/script.groovy
@@ -18,6 +18,7 @@ def buildImage() {
         dockerImage = docker.build registryName + ":$BUILD_NUMBER"
     //}
     } 
+*/
 
 def pushImage() {
     echo 'Pushing the build image...'
@@ -25,7 +26,7 @@ def pushImage() {
          dockerImage.push()
          }
 } 
-*/
+
 
 def destroy() {
        echo 'removing previously build container'
